8afcf34a9d8243dab49601d1a7826cc515c8f180
David Schneider <david.jay.schneider@gmail.com>
2019-12-20 10:53:24 -0500

    adds updates to serverResponse

--- hello-http/index.js
+++ hello-http/index.js
@@ -1,10 +1,38 @@
 
 const http = require('http');
 
+const fs = require('fs');
+
 const server = http.createServer((req, res) => {
+
     console.log('Oh wow, I got a request.');
-})
+    console.log(req.url);
+    console.log(req.method);
+
+    let url = req.url;
+    if (url === '/') {
+        url = '/index.html';
+    }
+
+    // Read the HTML file's contents, so we can send it in the response.
+    fs.readFile(`${__dirname}/public${url}`, (error, buf) => {
+        if (error) {
+            console.log(error);
+            res.writeHead(404, {
+                'Content-Type': 'text.html'
+            });
+            res.end(`<h1>File not found</h1>`);
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'text.html'
+            });
+            const contents = buf.toString();
+            res.end(contents);
+        }
+    });
+});
 
 server.listen(3000, () => {
     console.log('Server working.');
-}
+});
+
